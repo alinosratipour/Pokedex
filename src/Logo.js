cceab08058a3607c2594008e46418d5bbd89c050
alinosratipour <alinosratipour@gmail.com>
2021-04-30 13:49:25 +0100

    added hover effect on button

--- src/Logo.js
+++ src/Logo.js
@@ -4,7 +4,7 @@ const Logo = ({HandelClick}) =>{
 
   return(
     <header>
-        <h1>pokedex App</h1>
+        <h1 className="title">pokedex App</h1>
       <img onClick={HandelClick} alt="pokemon" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/016.png"></img>
     </header>);
 }
